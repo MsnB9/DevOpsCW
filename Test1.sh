0af620590d217d9fa8b20f11e277c20d09b45e83
“MsnB9” <mbilal204@caledonian.ac.uk>
2022-11-18 14:33:27 +0000

    Updating script 1

--- Test1.sh
+++ Test1.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-echo "Test 1 - no input"
+echo "Test 1 (a) - Testing script with no input given"
 input1=""
 ExpectedOutput="Please insert value"
 #running java code command
 javaOutput=$(java CurrencyConverter $input1 2>&1)
 if [ "$javaOutput" = "$ExpectedOutput" ]; then
-echo "Test 1 pass"
+echo "Test 1 with no input passed"
 else
-echo "Test 1 failed"
+echo "Test 1 with no value given failed"
 fi
 
 
